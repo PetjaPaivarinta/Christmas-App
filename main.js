869e17dc46c7dd18f5c89205114017bad08ae716
PetjaPaivarinta <Petja.Paivarinta@proton.me>
2024-11-05 10:07:06 +0200

    removed the menu

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, Menu } = require('electron/main')
+
+Menu.setApplicationMenu(null)
 
 const createWindow = () => {
   const win = new BrowserWindow({
